b2269139e145edcee8838389a16835a10925eae2
John Vazquez <johnnyvcoding@gmail.com>
2021-01-15 12:09:08 -0600

    added ls func

--- bash.js
+++ bash.js
@@ -1,10 +1,16 @@
+let directory = require('./pwd')
+let ls = require('./ls')
+
 process.stdout.write('prompt > ');
 
 process.stdin.on('data', (data) => {
   const cmd = data.toString().trim();
 
   if (cmd === 'pwd'){
-    process.stdout.write(process.cwd());
+    directory();
+  }
+  else if (cmd === 'ls'){
+    ls();
   }
 
 });
---
+++ pwd.js
@@ -0,0 +1,7 @@
+function getDirectory (){
+  process.stdout.write(process.cwd());
+}
+
+
+
+module.exports = getDirectory;
